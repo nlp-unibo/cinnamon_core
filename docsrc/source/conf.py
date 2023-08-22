5a334af1206851cfa502bb49657e2503cfe1d66e
Federico Ruggeri <federugg26@gmail.com>
2023-08-22 11:12:15 +0200

    updated sphinx theme

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -53,6 +53,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_pdj_theme'
-html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_theme = 'pydata_sphinx_theme'
+# html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 html_static_path = ['_static']
